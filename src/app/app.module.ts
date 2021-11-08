0430b75115a9da4f6cca7cd01b2b1e3914ac034c
hoftim <92081801+hoftim@users.noreply.github.com>
2021-11-08 09:58:37 +0100

    create services-folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProgressbarComponent } from './components/progressbar/progressbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    ProgressbarComponent
   ],
   imports: [
     BrowserModule,
