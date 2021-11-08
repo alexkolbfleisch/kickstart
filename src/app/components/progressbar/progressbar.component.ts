17e4d37cd4dfbe32de6afae7c382f909e38b79c9
hoftim <92081801+hoftim@users.noreply.github.com>
2021-11-08 14:11:46 +0100

    add progressbar

--- src/app/components/progressbar/progressbar.component.ts
+++ src/app/components/progressbar/progressbar.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbProgressbarConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-progressbar',
   templateUrl: './progressbar.component.html',
-  styleUrls: ['./progressbar.component.css']
+  styleUrls: ['./progressbar.component.css'],
+  providers: [NgbProgressbarConfig],
 })
 export class ProgressbarComponent implements OnInit {
+  _value: number = 0;
+  _topics: String[] = [];
+  _numTopics: number = 0;
 
-  constructor() { }
+  constructor(config: NgbProgressbarConfig) { 
+    config.max = this._numTopics;
+    config.striped = false;
+    config.animated = true;
+    config.type = 'success';
+    config.height = '20px';
+  }
 
   ngOnInit(): void {
+    this._topics = ["","Start","Java","Business","HTML", "CSS","DB", "Ende",""];
+    this._numTopics = this._topics.length+2;
+  }
+
+  fillProgress(fill: number){
+    this._value =  fill;
   }
 
 }
