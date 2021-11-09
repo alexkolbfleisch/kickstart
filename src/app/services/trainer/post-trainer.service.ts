9fdee9b9175d8c563ca64e861b5b1125d8668a10
hoftim <92081801+hoftim@users.noreply.github.com>
2021-11-09 15:15:23 +0100

    updated Trainer-service&-model

--- src/app/services/trainer/post-trainer.service.ts
+++ src/app/services/trainer/post-trainer.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Trainer } from '../../model/Trainer';
+
+const baseUrl = "";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostTrainerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+
+  list(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  get(id: string): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data: Trainer): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+
+  //extra
+  update(id: string, data: Trainer): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
 }
