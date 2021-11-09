9f98d10f9f633ad959b3bc0fab812964c79350ac
tisex <till.krueger@accenture.com>
2021-11-09 09:18:16 +0100

    add password component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProgressbarComponent } from './components/progressbar/progressbar.component';
+import { PasswordComponent } from './components/password/password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CalendarComponent,
-    ProgressbarComponent
+    ProgressbarComponent,
+    PasswordComponent
   ],
   imports: [
     BrowserModule,
     NgbModule, 
     CommonModule,
+    HttpClientModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
---
+++ src/app/components/password/password.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { getDefaultLibFilePath } from 'typescript';
+
+@Component({
+  selector: 'app-password', 
+  templateUrl: './password.component.html',
+  styleUrls: ['./password.component.css']
+})
+export class PasswordComponent implements OnInit {
+  
+  auth: string = ""; 
+  response: any = {
+    statusCode: 204,
+    headers: {
+        "Access-Control-Allow-Headers" : "Content-Type",
+        "Access-Control-Allow-Origin": "https://www.example.com",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    }
+  }
+  
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+   
+    this.getAuth();
+    console.log(this.auth);
+}
+getAuth() {
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+  const headers = {"Content-Type": "application/json","Access-Control-Allow-Origin" : "*"};
+  const pw = { pw:"MyAdmin2021" };
+
+  //this.http.options<any>('https://9xv557dk36.execute-api.eu-central-1.amazonaws.com/default/logIn-py');
+  return this.http.post<any>('https://9xv557dk36.execute-api.eu-central-1.amazonaws.com/default/logIn-py',(pw) ,httpOptions ).subscribe(data => {
+      this.auth = JSON.stringify(data);
+  });
+}
+
+test(): Observable<any> {
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+  const headers = {"Content-Type": "application/json","Access-Control-Allow-Origin" : "*"};
+  const pw = { pw:"MyAdmin2021" };
+  return this.http.post('https://9xv557dk36.execute-api.eu-central-1.amazonaws.com/default/logIn-py', pw);
+}
+
+}
